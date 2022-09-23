e42b7e0f5c3a563c8d8603062fecfba82997326d
Yuksel Argin <yukselargin@yahoo.com>
2022-09-23 16:40:38 +0300

    Initial commit

---
+++ src/Alan.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Alan {
+    public static void main(String[] args){
+        int a,b,c,u;
+        double alan;
+        Scanner kenar= new Scanner(System.in);
+
+        System.out.print("İlk kenarı giriniz:");
+        a= kenar.nextInt();
+        System.out.print("İlk kenarı giriniz:");
+        b= kenar.nextInt();
+        System.out.print("İlk kenarı giriniz:");
+        c= kenar.nextInt();
+
+        u= (a+b+c)/2;
+        alan = Math.sqrt(u*(u-a)*(u-b)*(u-c));
+        System.out.print("Üçgenin Alanı:"+alan);
+    }
+}
